20ffc3bca2db57124360a53fde10c13acc2137b0
silvia debussi <silviadebussi2@gmail.com>
2025-05-23 16:16:06 -0300

    SCRUM-6 criar constante

--- PHP-Analyzer-master/app/Http/Controllers/FileResultsController.php
+++ PHP-Analyzer-master/app/Http/Controllers/FileResultsController.php
@@ -6,41 +6,45 @@ use SegWeb\File;
 use Illuminate\Support\Facades\DB;
 
 class FileResultsController extends Controller {
+    private const ORDER_BY_LINE = 'line_number ASC';
+
     public function getAllByFileId($file_id) {
         $file = File::find($file_id);
         $return_collection = [];
-        if($file->type == 'File') {
+
+        if ($file->type == 'File') {
             $return_collection[] = DB::table('file_results')
-                    ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
-                    ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
-                    ->where('file_results.file_id', $file_id)
-                    ->orderByRaw('line_number ASC')
-                    ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
+                ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
+                ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
+                ->where('file_results.file_id', $file_id)
+                ->orderByRaw(self::ORDER_BY_LINE)
+                ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
         } else {
             $fileController = new FileController();
             $github_files = $fileController->getGithubFiles($file_id);
-            
-            if(!empty($github_files)) {
-                foreach($github_files as $github_file) {
+
+            if (!empty($github_files)) {
+                foreach ($github_files as $github_file) {
                     $return_collection[] = DB::table('file_results')
-                    ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
-                    ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
-                    ->where('file_results.file_id', $github_file->id)
-                    ->orderByRaw('line_number ASC')
-                    ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
+                        ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
+                        ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
+                        ->where('file_results.file_id', $github_file->id)
+                        ->orderByRaw(self::ORDER_BY_LINE)
+                        ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
                 }
             }
         }
+
         return $return_collection;
     }
 
     public function getSingleByFileId($file_id) {
         return DB::table('file_results')
-                ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
-                ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
-                ->where('file_results.file_id', $file_id)
-                ->orderByRaw('line_number ASC')
-                ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
+            ->leftJoin('terms', 'file_results.term_id', '=', 'terms.id')
+            ->leftJoin('term_types', 'terms.term_type_id', '=', 'term_types.id')
+            ->where('file_results.file_id', $file_id)
+            ->orderByRaw(self::ORDER_BY_LINE)
+            ->get(['line_number', 'term', 'term_type', 'color', 'file_id']);
     }
 
     public function showFileResults($file_id) {
@@ -48,21 +52,23 @@ class FileResultsController extends Controller {
         $file_results_controller = new FileResultsController();
         $fileController = new FileController();
         $files_ids = [];
-        if($file->type == 'File') {
+
+        if ($file->type == 'File') {
             $file_contents[$file->id]['content'] = FileController::getFileContentArray($file->id);
             $file_contents[$file->id]['results'] = $file_results_controller->getSingleByFileId($file->id);
             $file_contents[$file->id]['file'] = FileController::getFileById($file->id);
         } else {
             $github_files = $fileController->getGithubFiles($file_id);
-            if(!empty($github_files)) {
-                foreach($github_files as $github_file) {
+
+            if (!empty($github_files)) {
+                foreach ($github_files as $github_file) {
                     $file_contents[$github_file->id]['content'] = FileController::getFileContentArray($github_file->id);
                     $file_contents[$github_file->id]['results'] = $file_results_controller->getSingleByFileId($github_file->id);
                     $file_contents[$github_file->id]['file'] = FileController::getFileById($github_file->id);
                 }
             }
         }
+
         return view('file_results', compact(['file', 'file_contents']));
     }
 }
-
